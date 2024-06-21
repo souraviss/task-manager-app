aa8c466d16294aa3f0da337fba49acadc8010bb9
souraviss <sourav.iss2015@gmail.com>
2024-06-21 23:32:44 +0530

    final code

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -8,21 +8,20 @@ import {
 import { GetTasks } from "@/query/task"
 
 const Message = () => {
-  const { isPending, error, data: tasks, isFetching } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: async () => {
-      const { data, status } = await GetTasks();
-      return { data, status }
-    }
-  })
-console.log('data:',tasks);
+    const { isPending, error, data: tasks, isFetching } = useQuery({
+      queryKey: ['repoData'],
+      queryFn: async () => {
+        const { data, status } = await GetTasks();
+        return { data, status }
+
+      }
+    })
+
+
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 
-  // const {data,status} = await GetTasks();
-  // console.log("data", data);
-  // console.log("sttaus", status);
 
   return (
     <div className="h-screen">Message</div>
