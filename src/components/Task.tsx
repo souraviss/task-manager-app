3ee388db72fb50c03d10c1084ae2eb02939a7f35
souraviss <sourav.iss2015@gmail.com>
2024-07-19 22:21:18 +0530

    task ui changes

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -21,7 +21,7 @@ const Task = () => {
               href="/task"
             >
               <h2>Hello</h2>
-              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[90rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
                   {task.name}
                 </h3>
